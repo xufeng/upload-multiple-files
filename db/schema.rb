2f6f1af4efe6d51213a8c8007cd19a1631a0819f
xufeng <81959804@qq.com>
2012-03-01 21:53:26 +0800

    图片show修改

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120301123953) do
+ActiveRecord::Schema.define(:version => 20120301131157) do
+
+  create_table "pictures", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "file"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
